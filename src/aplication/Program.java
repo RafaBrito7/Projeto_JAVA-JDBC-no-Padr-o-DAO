1e3d3a774718df2f61e536e1d3ccf722b93b0b59
Rafael Brito <rafabrito.office@gmail.com>
2020-06-04 10:42:03 -0300

    Criado SellerDaoJDBC e DaoFactory

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,5 +1,7 @@
 package aplication;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 
 public class Program {
@@ -7,8 +9,9 @@ public class Program {
 	public static void main(String[] args) {
 
 		Department obj = new Department(1,"Books");
-		
 		System.out.println(obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 	}
 
 }
